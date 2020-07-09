41db31e1214ba1a227f023ce5b604681902cbd9d
Sheldon Lee <sheldon.m.lee@gmail.com>
2020-07-09 01:26:08 +0100

    Started in ui.

--- ui.c
+++ ui.c
@@ -1,1 +1,30 @@
+#include <stddef.h>
+#include <stdlib.h>
 #include "ui.h"
+
+typedef struct {
+	char* msg;
+	int* var;
+}Linei;
+
+static unsigned int ln_count = 0;
+
+static Linei* lines = NULL;
+
+void initUI()
+{
+	lines = (Linei*)malloc(sizeof(Linei) * ++ln_count);
+}
+
+void addLinei(char* msg, int* var)
+{
+	if (!lines) return;
+	if (ln_count >1) lines = realloc(lines, sizeof(Linei) * ++ln_count);
+	Linei line = { msg, var };
+	lines[ln_count-1] = line; 
+}
+
+void drawUI()
+{
+}
+
--- ui.h
+++ ui.h
@@ -1,6 +1,10 @@
 #ifndef UI_H
 #define UI_H
 
+void initUI();
 
+void addLinei(char* msg, int* var);
+
+void drawUI();
 
 #endif
